eff4151d41ecbf505fb8f55b3ebfd612de3186e3
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-05 06:02:01 +0530

    Add sample interfaces and objects
    Introduce src/interfaces_and_generic.ts which defines two TypeScript interfaces (coffee and team) and provides example objects (powder and ferrari) to illustrate typing for flavour/price and name/drivers_number.

---
+++ src/interfaces_and_generic.ts
@@ -0,0 +1,19 @@
+interface coffee {
+    flavour : string
+    price : number
+}
+
+const powder : coffee = {
+    flavour : "nanhe",
+    price : 123
+}
+
+interface team {
+    name : string,
+    drivers_number : number
+}
+
+const ferrari : team = {
+    name : 'charles leclerc',
+    drivers_number : 16
+}
