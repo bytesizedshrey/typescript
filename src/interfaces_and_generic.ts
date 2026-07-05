a3f077165c2a05d0e5a17694a73c0f8b58e66fc3
hey :) it’s shrey <110947127+bytesizedshrey@users.noreply.github.com>
2026-07-05 06:57:21 +0530

    Update interfaces_and_generic.ts

--- src/interfaces_and_generic.ts
+++ src/interfaces_and_generic.ts
@@ -16,4 +16,27 @@ interface team {
 const ferrari : team = {
     name : 'charles leclerc',
     drivers_number : 16
+} 
+
+interface teaMachine{
+    start() : void,
+    stop()  : void
+}
+
+const machine : teaMachine = {
+    start(){
+        console.log('start')
+    }, 
+    stop(){
+        console.log('stop')
+    }
+}
+
+interface charRating{
+    [flavor : string] : number
+}
+
+const rating : charRating = {
+    cool : 43,
+    roar : 32
 }
